2b76e04c428e814b5d5b6ac5513051b48b10c76e
unknown <andres.urdininea79@gmail.com>
2023-09-25 08:15:05 -0300

    Cambios en el index agregado el footer

---
+++ js/ejercicio1.js
@@ -0,0 +1,25 @@
+/Ejercicio 1/
+// Solicitar al usuario un numero a travez de un prompt
+// empezando desde 1 e ir incrementando este valor de 1
+// A medida que recorremos todos los numeros enteros anterior e
+// incluido el ingresado por el usuario quiero hacer un log de aquellos
+// numeros que son multiplos de 7
+
+// El log debe ser el siguiente
+// * El numero  X es multiplo de 7
+
+
+
+
+
+
+
+
+**/Ejercicio 2 /
+
+//*-Solicitar a una maestra que ingrese las clasificaciones de sus 
+//alumnos
+//Pedir constantemente las notas que la maestra ingrese un
+//0 como valor.En ese momento saldremos de nuestro bucle e
+//imprimiremos el promedio del curso segun las notas
+//ingresdas.
